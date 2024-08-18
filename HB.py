0101da6d0633a05caf02c26cf0b6134d6aa89515
dkpapp <118980727+dkpapp@users.noreply.github.com>
2024-08-18 20:23:16 +0530

    Update HB.py

--- HB.py
+++ HB.py
@@ -178,6 +178,7 @@ async def upload_repo(client, message):
 
         # Notify the user about successful upload
         await message.reply_text("Repository uploaded successfully: https://github.com/{m}/{repo}".format(m=g.me().login, repo=folder_name))
+        os.remove("repo.zip")
 
     except Exception as e:
         await message.reply_text("An error occurred: {}".format(str(e)))
